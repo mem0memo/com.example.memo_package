6e677e106873629b940df899883ab765e8b8cb30
Mamimu <sansangjmjg@gmail.com>
2026-06-25 16:56:21 +0900

    mm.unity.playerを実装した

---
+++ MM.Unity/MM.Unity.Player/PlayableGraphExtension.cs
@@ -0,0 +1,29 @@
+using mm.core.player;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace mm.unity.player
+{
+    public static class PlayableGraphExtension
+    {
+        public static CustomPlayer CreateOutputPlayer(
+            this PlayableGraph graph, IPlayControl playControl, GameObject gameObject = default)
+        {
+            var playableOutput = ScriptPlayableOutput.Create(graph, "PlayableOutput");
+            playableOutput.SetReferenceObject(gameObject);
+            var playable = ScriptPlayable<PlayablePlayBehaviour>.Create(graph);
+            playableOutput.SetSourcePlayable(playable);
+            var customPlayer = new CustomPlayer(playControl);
+            playable.GetBehaviour().SetControl(customPlayer);
+            return customPlayer;
+        }
+
+        public static ScriptPlayableOutput CreateOutput(
+            this PlayableGraph graph, string name, GameObject gameObject)
+        {
+            var output = ScriptPlayableOutput.Create(graph, name);
+            output.SetReferenceObject(gameObject);
+            return output;
+        }
+    }
+}
---
+++ MM.Unity/MM.Unity.Player/PlayablePlayBehaviour.cs
@@ -0,0 +1,21 @@
+using mm.core.player;
+using UnityEngine.Playables;
+
+namespace mm.unity.player
+{
+    public class PlayablePlayBehaviour : PlayableBehaviour
+    {
+        private PlayerBase player;
+
+        public void SetControl(PlayerBase player)
+        {
+            this.player = player;
+        }
+
+        public override void PrepareFrame(Playable playable, FrameData info)
+        {
+            var deltaTime = info.deltaTime;
+            player.UpdateTime(deltaTime);
+        }
+    }
+}
